5ae65a5dde9c085f2bcdcfecaa7d1185969dfaad
Tamirat worku Lemma <tamworku1@gmail.com>
2022-08-27 16:36:08 +0200

    added function to generate random uuid and char

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,37 @@
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/lexical_cast.hpp>
 
-namespace boost_uuid = boost::uuids;
-int main() {
+using std::cout;
+using std::endl;
+using std::string;
+using namespace boost::uuids;
 
-    const std :: string uuid_str = boost::lexical_cast<std::string>(boost::uuids::random_generator()());
-    std::cout << uuid_str << std::endl;
+string generate_random_uuid()
+{
+    string uuid_str = boost::lexical_cast<std::string>(boost::uuids::random_generator()());
+    return uuid_str;
+}
+
+//generate random char [0-9a-f]
+char generate_random_char()
+{
+    char c = rand() % 16;
+    if (c < 10)
+        c += '0';
+    else
+        c += 'a' - 10;
+    return c;
+}
+
+namespace boost_uuid = boost::uuids;
+int main() 
+{
+    srand(time(NULL));
+    std::cout << generate_random_uuid() << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << generate_random_char();
+    }
+    cout << endl;
     return 0;
 }
